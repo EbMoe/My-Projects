12272a3cab41d13705b2c6bd43cfc34b608ea602
Ebrahim Mohammed <ebmoemedia@gmail.com>
2025-02-11 18:19:27 +0200

    UI Home Screen

--- AlphaDelivery/app/build.gradle.kts
+++ AlphaDelivery/app/build.gradle.kts
@@ -58,4 +58,6 @@ dependencies {
     // Add Google Maps SDK
     implementation("com.google.android.gms:play-services-maps:19.0.0")
     implementation("com.google.android.gms:play-services-location:21.3.0")
+
+    implementation ("com.airbnb.android:lottie:5.0.0")
 }
