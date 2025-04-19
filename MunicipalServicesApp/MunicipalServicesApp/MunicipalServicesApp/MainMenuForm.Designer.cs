eb4effbe1caa77c8d1f52836d2e37072b4a7cc5a
Ebrahim Mohammed <ebmoemedia@gmail.com>
2025-04-19 20:51:58 +0200

    test

---
+++ MunicipalServicesApp/MunicipalServicesApp/MunicipalServicesApp/MainMenuForm.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace MunicipalServicesApp
+{
+    partial class MainMenuForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.lblAppName = new System.Windows.Forms.Label();
+            this.btnReportIssues = new System.Windows.Forms.Button();
+            this.btnLocalEvents = new System.Windows.Forms.Button();
+            this.btnServiceRequestStatus = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            
+            // lblAppName
+        
+            this.lblAppName.AutoSize = true;
+            this.lblAppName.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.lblAppName.ForeColor = System.Drawing.Color.White;
+            this.lblAppName.Location = new System.Drawing.Point(145, 20);
+            this.lblAppName.Name = "lblAppName";
+            this.lblAppName.Size = new System.Drawing.Size(182, 25);
+            this.lblAppName.TabIndex = 0;
+            this.lblAppName.Text = "Municipal App";
+            this.lblAppName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+           
+            // btnReportIssues
+            
+            this.btnReportIssues.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnReportIssues.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReportIssues.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnReportIssues.ForeColor = System.Drawing.Color.White;
+            this.btnReportIssues.Location = new System.Drawing.Point(20, 75);
+            this.btnReportIssues.Name = "btnReportIssues";
+            this.btnReportIssues.Size = new System.Drawing.Size(432, 75);
+            this.btnReportIssues.TabIndex = 1;
+            this.btnReportIssues.Text = "Report Issues";
+            this.btnReportIssues.UseVisualStyleBackColor = false;
+            this.btnReportIssues.Click += new System.EventHandler(this.btnReportIssues_Click);
+             
+            // btnLocalEvents
+            
+            this.btnLocalEvents.BackColor = System.Drawing.Color.Gray;
+            this.btnLocalEvents.Enabled = false;
+            this.btnLocalEvents.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLocalEvents.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnLocalEvents.ForeColor = System.Drawing.Color.White;
+            this.btnLocalEvents.Location = new System.Drawing.Point(20, 165);
+            this.btnLocalEvents.Name = "btnLocalEvents";
+            this.btnLocalEvents.Size = new System.Drawing.Size(432, 75);
+            this.btnLocalEvents.TabIndex = 2;
+            this.btnLocalEvents.Text = "Local Events and Announcements";
+            this.btnLocalEvents.UseVisualStyleBackColor = false;
+       
+            // btnServiceRequestStatus
+           
+            this.btnServiceRequestStatus.BackColor = System.Drawing.Color.Gray;
+            this.btnServiceRequestStatus.Enabled = false;
+            this.btnServiceRequestStatus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnServiceRequestStatus.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnServiceRequestStatus.ForeColor = System.Drawing.Color.White;
+            this.btnServiceRequestStatus.Location = new System.Drawing.Point(20, 255);
+            this.btnServiceRequestStatus.Name = "btnServiceRequestStatus";
+            this.btnServiceRequestStatus.Size = new System.Drawing.Size(432, 75);
+            this.btnServiceRequestStatus.TabIndex = 3;
+            this.btnServiceRequestStatus.Text = "Service Request Status";
+            this.btnServiceRequestStatus.UseVisualStyleBackColor = false;
+           
+            // MainMenuForm
+            
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.ClientSize = new System.Drawing.Size(472, 440);
+            this.Controls.Add(this.btnServiceRequestStatus);
+            this.Controls.Add(this.btnLocalEvents);
+            this.Controls.Add(this.btnReportIssues);
+            this.Controls.Add(this.lblAppName);
+            this.Name = "MainMenuForm";
+            this.Text = "Main Menu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAppName;
+        private System.Windows.Forms.Button btnReportIssues;
+        private System.Windows.Forms.Button btnLocalEvents;
+        private System.Windows.Forms.Button btnServiceRequestStatus;
+    }
+}
